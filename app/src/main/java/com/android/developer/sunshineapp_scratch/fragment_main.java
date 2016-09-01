ad820fdbd2e71be37e8fb290192e6e45bb297c43
Karthik <karthik.krishnaswamy17@gmail.com>
2016-09-01 14:59:01 +0530

    Created Sunshine APP Project and making the first commit.

---
+++ app/src/main/java/com/android/developer/sunshineapp_scratch/fragment_main.java
@@ -0,0 +1,29 @@
+package com.android.developer.sunshineapp_scratch;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class fragment_main extends Fragment {
+
+
+    public fragment_main() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
